4e2096ffc02c5b97973757278bfd17b97b0d8fc2
Dmitry <you.die.in.2012@yandex.ru>
2016-06-25 18:01:58 +0500

    NEW: Added constant manager

---
+++ app/src/main/java/com/softdesign/devintesive/utils/ConstantManager.java
@@ -0,0 +1,9 @@
+package com.softdesign.devintesive.utils;
+
+/**
+ * Created by dmitry on 23.06.16.
+ */
+public interface ConstantManager {
+    String TAG_PREFIX="DEV ";
+    String COLOR_MODE_KEY="COLOR_MODE_KEY";
+}
